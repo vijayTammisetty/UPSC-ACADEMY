16f8975504b0bec29b8a1167f205ca280d8c04dd
VijayKumar <vijayakumar.t328@gmail.com>
2024-05-01 14:19:08 +0530

    database changes

--- upscacademy/upscacademy/settings.py
+++ upscacademy/upscacademy/settings.py
@@ -80,14 +80,14 @@ WSGI_APPLICATION = 'upscacademy.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE' : 'django.db.backends.mysql',
-        'NAME': 'upscacademy',
-        'USER':'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE' : 'django.db.backends.mysql',
+        # 'NAME': 'upscacademy',
+        # 'USER':'root',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': '3306',
     }
 }
 
