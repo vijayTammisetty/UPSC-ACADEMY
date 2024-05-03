ff196c8eb8a83289893cf4c23576f99c294b5cdb
vijayTammisetty <120720424+vijayTammisetty@users.noreply.github.com>
2024-05-03 17:41:33 +0530

    Update settings.py

--- upscacademy/upscacademy/settings.py
+++ upscacademy/upscacademy/settings.py
@@ -140,4 +140,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_DEFAULT_ACL = None
+AWS_S3_FILE_OVERWRITE = False
+AWS_S3_VERITY = True
+# Optional: Specify S3 region explicitly
+AWS_S3_REGION_NAME = 'ap-south-1'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
